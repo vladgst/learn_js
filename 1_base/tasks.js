385c5ed32915ad4f38603fe4c540649a71d60325
vladgst <vlad.gst25@gmail.com>
2023-04-25 14:28:01 +0300

    tasks

--- 1_base/tasks.js
+++ 1_base/tasks.js
@@ -1,17 +1,29 @@
 // console.log("Hello")
 
 // 1 Вывод строки. Написать функцию, которая выводит получает строку и выводит ее в консоль 3 раза.
-
 // let message;
 // message = prompt('Напишите строку', '');
 // console.log(message.repeat(3))
 
 // 2 Вывод текста на экран.Написать функцию, которая выводит переданную строку (слово) на экран три раза, 
 // но в одной строке. Слова должны быть разделены пробелом и не должны сливаться в одно.
-
 // let message = prompt('Напишите строку', '');
 // function onWindowText() {
 //     return `${message} ${message} ${message}`
 // }
 // console.log(onWindowText(message))
 
+// 3 Минимум трех чисел. Написать функцию, которая вычисляет минимум из трёх чисел.
+// let number1 = 14
+// let number2 = 5
+// let number3 = 51
+// console.log(Math.min(number1, number2, number3));
+
+// 4 Дата рождения. Вывести на экран дату своего рождения в виде: MAY 1 2012
+// const myBirthDay = new Date('1998, 06, 06')
+// console.log(myBirthDay)
+
+// 5 Мой любимый учитель. Написать функцию, которая принимает имя учителя и выводит фразу.
+// Вывести на экран '<Имя учителя> мой любимый учитель' 1 раз.
+// const myFavouriteTeacher = 'Vlad'
+// console.log(`${myFavouriteTeacher} мой любимый учитель.`)
