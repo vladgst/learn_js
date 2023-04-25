6402e96b008129ef5eba2eb5dde4852547beb887
vladgst <vlad.gst25@gmail.com>
2023-04-25 12:49:43 +0300

    new files

---
+++ 1_base/index.js
@@ -0,0 +1,44 @@
+//alert ( 'Hello World')
+// let admin;
+// let name;
+// name = "Джон";
+// admin = name;
+// alert(admin);
+
+// const ourPlanet = "Earth";
+// const userOnline = userOnline;
+
+// let userName = prompt("Введите имя:")
+// alert(`Your name ${userName}`)
+
+// let a = prompt("Первое число?", 1);
+// let b = prompt("Второе число?", 2);
+// alert(+a + +b); // 12
+
+// let officialName = prompt("Какое официальное название JavaScript?", "")
+//
+//     if (officialName == "ECMAScript") {
+//         alert("Верно")
+//     }
+//     else {
+//         alert("Не верно")
+//     }
+
+// let number = prompt("Number", "")
+//     if (number > 0) {
+//         alert(1)
+//     }
+//         else if (number < 0) {
+//             alert(-1)
+//     }
+//         else if (number == 0) {
+//             alert(0)
+// }
+
+//let result = (a + b < 4) ? 'Мало' : 'Много';
+// let login = prompt('Введите логин', '')
+// let message = (login == 'Сотрудник') ? 'Привет' :
+//     (login == 'Директор') ? 'Здравстсвуйте' :
+//         (login == '') ? 'Нет логина' : ''
+
+
