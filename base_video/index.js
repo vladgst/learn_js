16d90b0e3f39d663328e08a849d1f9f1c3203773
vladgst <vlad.gst25@gmail.com>
2023-05-02 14:43:15 +0300

    up

--- base_video/index.js
+++ base_video/index.js
@@ -6,4 +6,31 @@
  // copyObjectA.c = '123'
  // console.log(objectA)
 
-  
+ //  const myCity = {
+ //    city: 'Kharkov',
+ //    info: {
+ //        isPopular: true,
+ //        country: 'Ukraine'
+ //    }
+ //  }
+ //  console.log(myCity.info.isPopular)
+ // delete myCity.info['isPopular']
+ // console.log(myCity)
+
+ // const person =  {
+ //    name: 'Vlad',
+ //     age: 25
+ // }
+ // // const person2 = Object.assign({}, person)
+ // // const person2 = { ...person }
+ // const person2 = JSON.parse(JSON.stringify(person))
+ // person2.age = 26
+ // console.log(person2.age)
+ // console.log(person.age)
+
+ // function printMyName() {
+ //    console.log('Vlad')
+ // }
+ // setTimeout(printMyName,1000)
+
+ 
